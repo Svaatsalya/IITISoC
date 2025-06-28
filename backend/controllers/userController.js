0f3e8efe5f522915c62957b8d5fcb1e53dcbb33b
Svaatsalya <vaatsalyasahu@gmail.com>
2025-06-28 20:27:23 +0530

    backend added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,101 @@
+import User from '../models/userModel.js'
+import bcrypt from "bcryptjs"
+import jwt from 'jsonwebtoken'
+
+
+//Generate a token jwt
+const generateToken = (userId) => {
+    return jwt.sign({id:userId}, process.env.JWT_SECRET,{expiresIn: '7d'})
+}
+
+export const registerUser = async (req,res) => {
+    try{
+        const{name,email,password} = req.body;
+
+        //To Check if user already exists
+        const userExists = await User.findOne({email})
+        if(userExists){
+            return res.status(400).json({message: "User already exists"})
+        }
+        if(password.length < 8){
+            return res.status(400).json({message: "Password must be atleast of 8 characters"})
+        }
+
+        //Hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedpassword = await bcrypt.hash(password,salt)
+
+        //Create A User
+        const user = await User.create({
+            name,
+            email,
+            password:hashedpassword
+        })
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token: generateToken(user._id)
+        })
+
+    }
+
+    catch (error){
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        })
+    }
+    
+}
+
+//login function
+export const loginUser = async (req,res) => {
+    try{
+        const {email,password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(500).json({message: "Invalid email or password"})
+        }
+
+        //compare the password
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+             return res.status(500).json({message: "Invalid email or password"})
+
+        }
+
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token: generateToken(user._id)
+        })
+    }
+   catch (error){
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        })
+    }
+
+
+}
+
+//GetUser profile function
+
+export const getUserProfile = async (req,res) =>{
+    try{
+        const user = await User.findById(req.user.id).select("-password")
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+        res.json(user)
+    }
+     catch (error){
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        })
+     }
+}
---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { connectDB } from './config/db.js';
+import userRouter from './routes/userRouter.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import resumeRouter from './routes/resumeRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors())
+
+//connet database
+connectDB();
+
+//middleware
+app.use(express.json());
+app.use('/api/auth',userRouter);
+app.use('/api/resume',resumeRouter)
+
+app.use('/uploads',
+    express.static(path.join(__dirname,'uploads'),{
+        setHeaders:(res,_path) =>{
+            res.set('Access-Control-Allow-Origin', 'http://localhost:5173')
+        }
+    })
+)
+//Routes
+
+app.get('/' , (req,res) =>{
+    res.send('API WORKING')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server Started on http://localhost:${PORT}`)
+})
