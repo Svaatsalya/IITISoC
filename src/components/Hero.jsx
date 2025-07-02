e69e3c1cb0f626c7288f6a7f4e6f42eeabd9bea9
Chetan Verma <coutcv28@gmail.com>
2025-07-02 22:34:25 +0530

    Responsiveness minor fixes in Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,34 +6,34 @@ import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 
 const Hero = () => {
-        useEffect(() => {
-            AOS.init({
-                duration:1500,
-                once:true,
-            })
+    useEffect(() => {
+        AOS.init({
+            duration: 1500,
+            once: true,
         })
+    })
 
 
-  return (
-    <main className='flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh - 6rem)]'>
-        <div className='max-w-xl ml-[5%] z-10 mt=[90%] md:mt-[60%] lg:mt-0'>
-            <div className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
-                <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1'>
-                    <i class='bx bx-diamond'></i>
-                    INTRODUCING
+    return (
+        <main className='flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh - 6rem)]'>
+            <div className='max-w-xl ml-[5%] z-10 mt=[90%] md:mt-[5%] lg:mt-0'>
+                <div className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
+                    <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1'>
+                        <i class='bx bx-diamond'></i>
+                        INTRODUCING
+                    </div>
                 </div>
-            </div>
-            {/*main heading */}
-            <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold tracking-wider my-8'>
-                Build Your Portfolio Instantly
-               </h1>
+                {/*main heading */}
+                <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold tracking-wider my-8'>
+                    Build Your Portfolio Instantly
+                </h1>
                 {/** description */}
 
-             <p className='text-base sm:text-lg tracking-wider text-grey-400 max-w-[25rem] lg:max-w-[30rem]'>
-                Your Personal Website — In Just a Few Clicks
-                <br/>
-                No coding. No design hassle. Just your story, your projects — beautifully published.
-                 </p>   
+                <p className='text-base sm:text-lg tracking-wider text-grey-400 max-w-[25rem] lg:max-w-[30rem]'>
+                    Your Personal Website — In Just a Few Clicks
+                    <br />
+                    No coding. No design hassle. Just your story, your projects — beautifully published.
+                </p>
 
                 {/** Buttons */}
 
@@ -46,14 +46,14 @@ const Hero = () => {
                         Get Started <i class='bx bx-link-external'></i>
 
                     </a>
-               </div>
-             </div>
+                </div>
+            </div>
 
-             {/* 3D Model */}
-              <Spline className='absolute lg:top-0 top-[20%] bottom-0 lg:left-[25%] sm:top-[1%] sm:left-[2%] h-full' scene="https://prod.spline.design/bciGGCCRi4N7uDsq/scene.splinecode" />
+            {/* 3D Model */}
+            <Spline className=' lg:top-0 top-[20%] bottom-0 sm:top-[1%] sm:left-[2%] h-full' scene="https://prod.spline.design/bciGGCCRi4N7uDsq/scene.splinecode" />
 
-    </main>
-  )
+        </main>
+    )
 }
 
 export default Hero
