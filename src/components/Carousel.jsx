08aa42633e37b823faf2667e5941d9dca0e88f86
Chetan Verma <coutcv28@gmail.com>
2025-07-02 22:38:11 +0530

    Updated UI of Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -67,7 +67,7 @@ export default function ImageSlider() {
   return (
     <section
       ref={sectionRef}
-      className="w-full bg-gradient-to-b from-[#471172] to-[#551b82] py-20 px-4 sm:px-6 lg:px-10 text-white "
+      className="w-full bg-gradient-to-b from-black via-[#551b82] to-black py-20 px-4 sm:px-6 lg:px-10 text-white "
     >
       <div className="max-w-6xl mx-auto ">
         <h2 className="text-center text-4xl font-bold mb-14 text-white">
